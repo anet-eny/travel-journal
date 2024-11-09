5af1c79fdb0f42ee0c59cf630e24ee28d252c6fd
anet-eny <anet.eny@gmail.com>
2024-11-09 20:40:12 +0100

    Create Navbar component and add FontAwesome icon

--- travel-journal/src/App.jsx
+++ travel-journal/src/App.jsx
@@ -1,7 +1,10 @@
+import Navbar from "./components/Navbar"
+
+
 export default function App() {
   return (
     <div>
-      <h2>Hello</h2>
+      <Navbar />
     </div>
   )
 }
---
+++ travel-journal/src/components/Navbar.jsx
@@ -0,0 +1,8 @@
+export default function Navbar() {
+    return (
+        <nav>
+            <i class="fa-solid fa-earth-americas"></i>
+            <h2>my travel journal.</h2>
+        </nav>
+    )
+}
