926c3b1ca6fb17a7a1aaac0ab20d772277467847
anet-eny <anet.eny@gmail.com>
2024-11-10 14:19:35 +0100

    Add mobile screen style to Card component

--- travel-journal/src/components/Card.jsx
+++ travel-journal/src/components/Card.jsx
@@ -1,15 +1,18 @@
 export default function Card() {
     return (
         <div className="card">
-            <div className="container">
+            <div className="card-container">
                 <img src="https://scrimba.com/links/travel-journal-japan-image-url" alt="" />
-                <div className="location-div">
-                    <h4 className="location">Japan</h4>
-                    <p>View on Google Maps</p>
+                <div className="text-container">
+                    <div className="location-container">
+                        <i class="fa-solid fa-location-dot"></i>
+                        <h4 className="location">Japan</h4>
+                        <a href="#">View on Google Maps</a>
+                    </div>
+                    <h1>Mount Fuji</h1>
+                    <h3>12 Jan, 2023 - 24 Jan, 2023</h3>
+                    <p>Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
                 </div>
-                <h1>Mount Fuji</h1>
-                <h3>12 Jan, 2023 - 24 Jan, 2023</h3>
-                <p>Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
             </div>
         </div>
     )
