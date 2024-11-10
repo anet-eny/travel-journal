5f32667e79af8a7850987bf667a47222b25e184e
anet-eny <anet.eny@gmail.com>
2024-11-10 13:33:29 +0100

    Create Card component and link it with App

--- travel-journal/src/App.jsx
+++ travel-journal/src/App.jsx
@@ -1,10 +1,11 @@
 import Navbar from "./components/Navbar"
-
+import Card from "./components/Card"
 
 export default function App() {
   return (
     <div>
       <Navbar />
+      <Card />
     </div>
   )
 }
